fe8c6914f801152f30b080a832d853924db5a15b
vifrost <67081596+vifrost@users.noreply.github.com>
2022-11-30 23:03:24 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -572,7 +572,8 @@ def publish_discovery(dev, sub=''):
             'pr_mode_val_tpl': '{{ value_json.preset }}',
             'pr_mode_cmd_t': 'kocom/livingroom/fan/set_preset_mode/command',
             'pr_mode_cmd_tpl': '{{ value }}',
-            'pr_modes': ['Off', 'Low', 'Medium', 'High'],
+#           'pr_modes': ['Off', 'Low', 'Medium', 'High'],    # 20221130 off 제거 'Off' is not a valid preset mode
+            'pr_modes': ['Low', 'Medium', 'High'],
             'pl_on': 'on',
             'pl_off': 'off',
             'qos': 0,
